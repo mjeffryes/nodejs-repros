19d9d9dd54afd388578c76bc0658c2ca016110ee
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-12-01 16:52:30 -0800

    Reproduced

--- index.ts
+++ index.ts
@@ -1,1 +1,77 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as eks from "@pulumi/eks";
+
+/// Helpers for IAM
+
+const managedPolicyArns: string[] = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+];
+
+// Creates a role and attches the EKS worker node IAM managed policies
+function createRole(name: string): aws.iam.Role {
+    const role = new aws.iam.Role(name, {
+        assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+            Service: "ec2.amazonaws.com",
+        }),
+    });
+
+    let counter = 0;
+    for (const policy of managedPolicyArns) {
+        // Create RolePolicyAttachment without returning it.
+        const rpa = new aws.iam.RolePolicyAttachment(`${name}-policy-${counter++}`,
+            { policyArn: policy, role: role },
+        );
+    }
+
+    return role;
+}
+
+/**
+ * Identical IAM for all NodeGroups: all NodeGroups share the same `instanceRole`.
+ */
+
+// Create example IAM roles and profiles to show to use them with NodeGroups.
+// Note, all roles for the instance profiles are required to at least have
+// the following EKS Managed Policies attached to successfully auth and join the
+// cluster:
+//   - "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+//   - "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+//   - "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+const role0 = createRole("example-role0");
+const instanceProfile0 = new aws.iam.InstanceProfile("example-instanceProfile0", {role: role0});
+
+// Create an EKS cluster with a shared IAM instance role to register with the
+// cluster auth.
+const cluster1 = new eks.Cluster("example-nodegroup-iam-simple", {
+    skipDefaultNodeGroup: true,
+    deployDashboard: false,
+    instanceRole: role0,
+});
+
+// There are two approaches that can be used to add additional NodeGroups.
+// 1. A `createNodeGroup` API on `eks.Cluster`
+// 2. A `NodeGroup` resource which accepts an `eks.Cluster` as input
+
+// Create the node group using an `instanceProfile` tied to the shared, cluster
+// instance role registered with the cluster auth through `instanceRole`.
+cluster1.createNodeGroup("example-ng-simple-ondemand", {
+    instanceType: "t3.medium",
+    desiredCapacity: 1,
+    minSize: 1,
+    maxSize: 2,
+    labels: {"ondemand": "true"},
+    instanceProfile: instanceProfile0,
+});
+
+const ng = new eks.NodeGroupV2("example-ng2-simple-ondemand", {
+    cluster: cluster1,
+    instanceType: "t3.medium",
+    desiredCapacity: 1,
+    minSize: 1,
+    maxSize: 2,
+    labels: {"ondemand": "true"},
+    instanceProfile: instanceProfile0,
+});
+
