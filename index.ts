6781f1c53981f9dd9fb413cdb08422820be2bc6b
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-12-05 16:20:56 -0800

    No Repro

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+
+const _ingresskNamespace = new k8s.core.v1.Namespace(
+  'ingress',
+  { metadata: { name: 'ingress' } },
+//  { provider: k8sProvider },
+)
+
+//const _ingressNginxValues = new pulumi.asset.FileAsset('charts/ingress-nginx/values.yaml')
+const _ingressNginxRelease = new k8s.helm.v3.Release(
+  'ingress-nginx',
+  {
+    name: 'ingress-nginx',
+    namespace: _ingresskNamespace.metadata.name,
+    chart: 'ingress-nginx',
+    repositoryOpts: { repo: 'https://kubernetes.github.io/ingress-nginx' },
+    version: '4.8.3',
+    //valueYamlFiles: [_ingressNginxValues],
+  },
+  { /*provider: k8sProvider, */dependsOn: [_ingresskNamespace] },
+)
