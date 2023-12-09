a2e98cd19a8a5d959ac8a4d0afedfa39d9bfa287
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-12-08 17:39:47 -0800

    repro

--- index.ts
+++ index.ts
@@ -1,1 +1,43 @@
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 import * as pulumi from "@pulumi/pulumi";
+
+// An ECS cluster to deploy into.
+const cluster = new aws.ecs.Cluster("cluster", {});
+
+// Create a load balancer to listen for requests and route them to the container.
+const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", { });
+
+// Create the ECR repository to store our container image
+const repo = new awsx.ecr.Repository("repo", {
+    forceDelete: true,
+});
+
+// Build and publish our application's container image from ./app to the ECR repository.
+const image = new awsx.ecr.Image("image", {
+    repositoryUrl: repo.url,
+    context: "./app",
+});
+
+// Define the service and configure it to use our image and load balancer.
+const service = new awsx.ecs.FargateService("service", {
+    cluster: cluster.arn,
+    assignPublicIp: true,
+    taskDefinitionArgs: {
+        container: {
+            name: "awsx-ecs",
+            image: image.imageUri,
+            cpu: 128,
+            memory: 512,
+            essential: true,
+            portMappings: [{
+                hostPort: 3000,
+                containerPort: 3000,
+                targetGroup: loadbalancer.defaultTargetGroup,
+            }],
+        },
+    },
+});
+
+// Export the URL so we can easily access it.
+export const frontendURL = pulumi.interpolate `http://${loadbalancer.loadBalancer.dnsName}`;
